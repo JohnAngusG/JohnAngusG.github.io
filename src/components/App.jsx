86fe98f7c996e840c9dc61a687f83df99bbab88e
John <j.a.mullaney1@gmail.com>
2025-03-06 17:07:57 -0800

    Add components folder...

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+const App = () => (
+    <>
+        <div className="navbar bg-base-100 shadow-sm">
+            <a className="btn btn-ghost text-xl">John Mullaney</a>
+        </div>
+        <div className="hero bg-base-300 min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Hi, I'm John</h1>
+                    <p className="py-6">
+                        My experiences with game development, web development, and networking allow me to excel in any scenario. 
+                        In my free time, I can usually be found reading, tinkering, and watching basketball.
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-2">
+            
+        </div>
+    </>
+)
+
+export default App;
---
+++ src/components/MyNav.jsx
@@ -0,0 +1,6 @@
+const MyNav = () => (
+    <div className="navbar bg-base-100 shadow-sm">
+        <a className="btn btn-ghost text-xl">John Mullaney</a>
+    </div>
+)
+export default MyNav;
