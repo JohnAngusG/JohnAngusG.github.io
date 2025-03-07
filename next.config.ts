c644787229391d9d60b2efe3ddb94b92bb66174e
John <j.a.mullaney1@gmail.com>
2025-03-06 16:46:28 -0800

    Refactor github action

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
-// next.config.js
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 let assetPrefix = '';
 let basePath = '';
 
 if (isGithubActions) {
-  // trim off `<repo-name>/` from repo url
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  const repo = (process.env.GITHUB_REPOSITORY || '').replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
 const nextConfig = {
@@ -17,6 +15,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  output: 'export',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
