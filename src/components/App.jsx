4a18c479949073f94c19ac0711eef100e53e3b74
John <j.a.mullaney1@gmail.com>
2025-03-07 18:15:36 -0800

    Add cards to site with links to projects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+
+
 const App = () => (
     <>
         <div className="navbar bg-base-100 shadow-sm">
@@ -15,9 +17,38 @@ const App = () => (
                 </div>
             </div>
         </div>
-        <div className="flex flex-row justify-center items-center gap-2">
-            
+        <div className="w-full max-w-screen-xl flex flex-col md:flex-row justify-center items-center gap-4">
+        <div className="card bg-base-300 w-96 shadow-sm">
+            <figure>
+                <img
+                    src="images/lucky&zombie.png"
+                    alt="Cartoon dog with cartoon zombie in the background. Don't worry, the dog can't get hurt." />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">DogPocalypse</h2>
+                <p>You play as lucky and hide from zombies while collecting food for your human.</p>
+                <div className="card-actions justify-end">
+                    <a href="https://johnmullaney.tech/DogBuildWeb/" target="_blank" className="btn btn-primary">Play Now</a>
+                    {/* <button className="btn btn-secondary">Thoughts</button> */}
+                </div>
+            </div>
         </div>
+        <div className="card bg-base-300 w-96 shadow-sm">
+            <figure>
+                <img
+                    src="images/tetrisFixed.png"
+                    alt="Cartoon dog with cartoon zombie in the background. Don't worry, the dog can't get hurt." />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Tetris</h2>
+                <p>Tetris written completely in C++ using SFML. Includes ability to see piece previews and instantly drop them.</p>
+                <div className="card-actions justify-end">
+                    <a href="https://github.com/JohnAngusG/Tetris" target="_blank" className="btn btn-primary">Play Now</a>
+                    {/* <button className="btn btn-secondary">Thoughts</button> */}
+                </div>
+            </div>
+        </div>
+    </div>
     </>
 )
 
